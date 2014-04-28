12e815e8bce4a8011b1147deca7cd1af5b258b45
Scott Li <scottli0101@gmail.com>
2014-04-28 09:40:19 -0700

    fixed form

--- app/assets/javascripts/controllers/accountCtrl.js
+++ app/assets/javascripts/controllers/accountCtrl.js
@@ -1,33 +1,31 @@
 'use strict'
 
-tcApp.controller( "accountCtrl", function( $scope, $resource, Account, $location ) {
+tcApp.controller("accountCtrl", function($scope, $resource, Account, $location) {
 	// Default states
 	$scope.shown = false
 	$scope.sending = false
+	$scope.withdraw = {}
 
-	Account.get( "/", function( account ) {
+	Account.get("/", function(account) {
 		$scope.account = account
 		$scope.shown = true
 		$scope.submitWithdraw = function() {
-			if ( $scope.withdrawForm.$invalid ) {
-				return;
-			}
 
 			$scope.sending = true
-			account.$withdraw( {
+			account.$withdraw({
 				toAddress: $scope.withdraw.toAddress,
 				withdrawAmount: $scope.withdraw.amount,
-			}, function( response ) {
+			}, function(response) {
 				$scope.sending = false
 				$scope.withdrawFailed = false
 				$scope.account = response
-			}, function( error ) {
-				console.log( error )
+			}, function(error) {
+				console.log(error)
 				$scope.sending = false
 				$scope.withdrawFailed = true
-			} )
+			})
 		}
-	}, function( error ) {
-		$location.path( "/" )
-	} )
-} )
+	}, function(error) {
+		$location.path("/")
+	})
+})
