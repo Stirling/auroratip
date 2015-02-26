8cb3baa0a507acab466872b3cc70892ad50ac7b0
Nick Verwymeren <me@nickv.codes>
2015-02-26 14:15:17 -0600

    updated usd conversion address

--- app/services/bitstamp.rb
+++ app/services/bitstamp.rb
@@ -2,7 +2,7 @@ module Bitstamp
   extend self
 
   def latest
-    response = HTTParty.get("http://api.cryptocoincharts.info/tradingPair/aur_usd")
-    response["price"].to_f
+    response = HTTParty.get("https://www.cryptonator.com/api/ticker/aur-usd")
+    response["ticker"]["price"].to_f
   end
 end
