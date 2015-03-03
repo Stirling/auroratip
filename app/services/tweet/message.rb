6937198a1c6d705c5e2fac9da8032b35c0ff0152
Nick Verwymeren <me@nickv.codes>
2015-03-03 10:19:27 -0600

    updated url

--- app/services/tweet/message.rb
+++ app/services/tweet/message.rb
@@ -4,7 +4,7 @@ module Tweet::Message
     extend self
 
     def recipient(recipient, sender, amount)
-      link = "www.auroratip.com/#/profile/#{recipient}?direct=true&r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/#/profile/#{recipient}?direct=true&r=#{Helper.rand()}"
       "@#{recipient}, @#{sender} just tipped you #{amount.to_BTCStr} BTC! "\
       "See it here #{link}"
     end
@@ -14,7 +14,7 @@ module Tweet::Message
     extend self
 
     def unauthenticated(sender)
-      link = "www.auroratip.com/auth/twitter?r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/auth/twitter?r=#{Helper.rand()}"
       "@#{sender}, to start tipping, please authenticate via twitter "\
       "and make a deposit. Thanks! #{link}"
     end
@@ -28,33 +28,33 @@ module Tweet::Message
     end
 
     def likely_forgot_symbol(sender)
-      link = "www.auroratip.com/#/how-it-works?r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/#/how-it-works?r=#{Helper.rand()}"
       "@#{sender}, apologies human, I cannot compute your tweet :( Please see #{link}"
     end
 
     # TODO: Include link with amount
     def not_enough_balance(sender)
-      link = "www.auroratip.com/auth/twitter?r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/auth/twitter?r=#{Helper.rand()}"
       "@#{sender}, please top up on your account before sending this tip. #{link}"
     end
 
     def enough_confirmed_unspents(sender)
-      link = "www.auroratip.com/#/account/deposit?r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/#/account/deposit?r=#{Helper.rand()}"
       "@#{sender}, you don't have enough confirmed unspents, pls wait for a few mins! #{link}"
     end
 
     def min_output(sender)
-      link = "www.auroratip.com/#/documentation?r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/#/documentation?r=#{Helper.rand()}"
       "@#{sender}, amount must be 0.01 AUR or above #{link}"
     end
 
     def zero_amount(sender)
-      link = "www.auroratip.com/#/how-it-works?r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/#/how-it-works?r=#{Helper.rand()}"
       "@#{sender}, please tip 0.01 AUR or more. Refer to #{link}"
     end
 
     def unknown(sender)
-      link = "www.auroratip.com/#/how-it-works?r=#{Helper.rand()}"
+      link = "auroratip.auroracoin.io/#/how-it-works?r=#{Helper.rand()}"
       "@#{sender}, sorry, I'm not sure what you meant :s. Please refer to #{link}"
     end
 
@@ -64,7 +64,7 @@ module Tweet::Message
     extend self
 
     def reminder(recipient, amount)
-      link = "www.auroratip.com/#/profile/#{recipient}?direct=true"
+      link = "auroratip.auroracoin.io/#/profile/#{recipient}?direct=true"
       # sendersStr = sendersArr.map {|s| "@#{s}"}.join(", ")
 
       "@#{recipient}, you have #{amount} AUR pending in tips! "\
